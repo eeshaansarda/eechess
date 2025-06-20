d94a343354b52f052b1563e4a8888d3041eaeae0
Eeshaan Sarda <eeshaansarda98@gmail.com>
2025-06-20 19:01:35 +0530

    added shadcn and completed boilerplate stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Tailwind working</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
